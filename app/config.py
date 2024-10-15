545e4cacc7636bb89b7cdeec3a94e84de191f6ba
Mike <129897918+RedParrotBerkeley@users.noreply.github.com>
2024-10-15 15:31:20 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 # Set the API key for the OpenAI client
 openai.api_key = openai_api_key
 
-MYSQL_DATABASE = 'debatebot_dev'
+MYSQL_DATABASE = 'debatebot_prod'
 
 class Settings(BaseSettings):
     # App settings
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
     mysql_host: str = os.getenv("MYSQL_HOST")
     mysql_port: int = int(os.getenv("MYSQL_PORT", 3306))
 
-    mysql_database: str = "debatebot_dev"
+    mysql_database: str = "debatebot_prod"
 
 
     # OpenAI settings
@@ -32,11 +32,11 @@ class Settings(BaseSettings):
     openai_model: str = "gpt-4o-mini"
     
     # Auth0 settings
-    auth0_domain: str = os.getenv("AUTH0_DOMAIN", "https://hrf-alt-dev.us.auth0.com")
+    auth0_domain: str = os.getenv("AUTH0_DOMAIN", "https://hrf-production.us.auth0.com/")
     auth0_client_id: str = os.getenv("AUTH0_CLIENT_ID")
     auth0_client_secret: str = os.getenv("AUTH0_CLIENT_SECRET")
-    auth0_audience: str = os.getenv("AUTH0_AUDIENCE", "https://dbapi-stag.hrfinnovation.org/api/v2/")
-
+    auth0_audience: str = os.getenv("AUTH0_AUDIENCE", "https://dbapi.hrfinnovation.org/api/v2/")
+    
     class Config:
         env_file = ".env"
 
